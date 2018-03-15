24676b834c931a618fdfddbfb9becbb86d865538
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-03-15 14:23:59 +0300

    рендеринг видео на canva 2d
    FPS 50-60

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
 const player = document.getElementById('player');
+const canvas = document.getElementById('canvasForVideo');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 navigator.mediaDevices.getUserMedia({ audio: true, video: true })
   .then((stream) => {
     player.src = URL.createObjectURL(stream);
   });
+
+function draw(video, canvasContext) {
+  canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(() => {
+    draw(video, canvasContext);
+  });
+}
+
+draw(player, ctx);
