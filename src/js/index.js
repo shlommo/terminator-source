f557389faf725419a729ea677f0abe7a3c5951e6
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-03-15 13:44:11 +0300

    тег видео и захват стрима камеры

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-console.log(123);
+const player = document.getElementById('player');
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+  .then((stream) => {
+    player.src = URL.createObjectURL(stream);
+  });
