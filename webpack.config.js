8afdf4760fb0cd26960ab78324eaaa7424691f91
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-03-18 23:36:02 +0300

    иправляет стилистические ошибки в конфиге webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const fs = require('fs');
 
 function generateHtmlPlugins(templateDir) {
   const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
+  return templateFiles.map((item) => {
     const parts = item.split('.');
     const name = parts[0];
     const extension = parts[1];
     return new HtmlWebpackPlugin({
       filename: `${name}.html`,
       template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-      inject: false,
-    })
-  })
+      inject: false
+    });
+  });
 }
 
 const htmlPlugins = generateHtmlPlugins('./src/html/views');
@@ -29,51 +29,49 @@ module.exports = {
   output: {
     filename: './js/bundle.js'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [{
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: 'env'
-          }
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src/js'),
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: 'env'
         }
-      },
-      {
-        test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, 'src/scss'),
-        use: ExtractTextPlugin.extract({
-          use: [{
-              loader: "css-loader",
-              options: {
-                sourceMap: true,
-                minimize: true,
-                url: false
-              }
-            },
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
-      },
-      {
-        test: /\.html$/,
-        include: path.resolve(__dirname, 'src/html/includes'),
-        use: ['raw-loader']
-      },
-    ]
+      }
+    },
+    {
+      test: /\.(sass|scss)$/,
+      include: path.resolve(__dirname, 'src/scss'),
+      use: ExtractTextPlugin.extract({
+        use: [{
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+            minimize: true,
+            url: false
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }]
+      })
+    },
+    {
+      test: /\.html$/,
+      include: path.resolve(__dirname, 'src/html/includes'),
+      use: ['raw-loader']
+    }]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new ExtractTextPlugin({
       filename: './css/style.min.css',
-      allChunks: true,
+      allChunks: true
     }),
     new CopyWebpackPlugin([{
       from: './src/fonts',
